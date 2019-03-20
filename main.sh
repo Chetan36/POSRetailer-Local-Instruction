dc326db8ef9e3f4c7cde67f35e6138c6fc3dae74
Sarbe <sarbe97@gmail.com>
2019-03-21 02:12:46 +0530

    files added

---
+++ main.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+CODE_DIR=$HOME/POS/code
+#PRD_DIR=$HOME/POS/prd
+
+GIT_DIR=POSRetailer-Local
+#1. Create codebase dir
+if [ ! -d $CODE_DIR ] 
+then
+	echo "dir structure not present. creating $CODE_DIR"
+	mkdir -p $CODE_DIR
+fi
+
+
+#########
+# 2. Create actual prd dir
+if [ ! -d $PRD_DIR ] 
+then
+	echo "dir structure not present. creating $PRD_DIR"
+	mkdir -p $PRD_DIR
+fi
+##########
+
+
+cd $CODE_DIR
+
+if [ -d $CODE_DIR/$GIT_DIR ] 
+then
+	echo "Removing exisiting GIT dir $GIT_DIR"
+	rm -r -f $GIT_DIR
+fi
+
+echo 'Cloning Repository from GIT for first time.'
+git clone https://github.com/Chetan36/POSRetailer-Local.git
+
+
+
+
+#Installation Process
+
+#4. check if mvn present else install
+node -v
+rc=$?
+if [ $rc != 0 ] # Not Present
+then 
+	echo "maven not present. Installing maven ..."
+	sudo apt-get install nodejs
+fi
+
+#i
+npm install -g pm2
+
+
+#ii.. go to POSRetailer-Local folder
+cd $CODE_DIR/$GIT_DIR||exit
+
+#iv stop and
+pm2 stop all && pm2 delete all
+
+#iii
+npm install
+#iv start server
+pm2 start posRetailer.js
+
+
+# ii. Properties file if any
+cp start.html $HOME/Desktop/WareHouse.html
+
+#remove git dir
+#cd $CODE_DIR
+#sudo rm -r -f *
+exit
+
+
+
+
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-POS_HOME=$HOME/POS
-GIT_DIR=Controller
+POS_HOME=$HOME/POS/scripts
+GIT_DIR_INSTR=POSRetailer-Local-Instruction
+#Controller
 
 #1.go to control folder
 if [ ! -d $POS_HOME ]
@@ -10,14 +11,14 @@ fi
 
 cd $POS_HOME
 
-if [ -d $POS_HOME/$GIT_DIR ] 
+if [ -d $POS_HOME/$GIT_DIR_INSTR ] 
 then
 	echo 'Fetching updated from GIT'
-	cd $POS_HOME/$GIT_DIR
+	cd $POS_HOME/$GIT_DIR_INSTR
 	git pull
 else 
 	echo 'Cloning Repository from GIT for first time.'
-	git clone https://github.com/Sarbe/Controller.git
+	git clone https://github.com/Chetan36/POSRetailer-Local-Instruction.git
 fi
 
 rc=$?
@@ -26,7 +27,10 @@ then
 	echo "Please check internet connection"
 	exit
 fi
-#3.. go to Rxdfert folder
-cd $POS_HOME/$GIT_DIR||exit
+#3.. go to GIT_DIR_INSTR folder
+cd $POS_HOME/$GIT_DIR_INSTR||exit
+# Run control command
+chmod 755 main.sh
 sh -x main.sh
+
 exit
